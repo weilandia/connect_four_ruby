cc9edba6f8175b2e930e1c726785112be0d4f405
Nick Weiland <nickweiland@gmail.com>
2018-05-13 10:39:07 +0200

    Implements flexible rowxcolumns

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,31 @@
+require "pry"
 class Game
+  attr_reader :rows, :columns
+
+  def initialize(rows: 6, columns: 7)
+    @rows = rows
+    @columns = columns
+  end
+
+  def winning_positions
+    [
+      horizontal_wins,
+      vertical_wins,
+      diagonal_wins
+    ].flatten(1)
+  end
+
   def render_board
-    output = "#{"-" * 33}\n"
-    0.upto(41) do |position|
+    output = "#{"-" * (columns * 5 - 2)}\n"
+    0.upto(rows * columns - 1) do |position|
       if position <= 9
         output << " #{position}  "
       else
         output << " #{position} "
       end
 
-      if (position + 1) % 7 == 0
-        output << "\n#{"-" * 33}\n"
+      if (position + 1) % columns == 0
+        output << "\n#{"-" * (columns * 5 - 2)}\n"
       else
         output << "|"
       end
@@ -18,7 +34,40 @@ class Game
   end
 
   private
+
+    def total_positions
+      rows * columns
+    end
+
+    def last_index
+      total_positions - 1
+    end
+
+    def horizontal_wins
+      row_starts.map do |row_start|
+        row_start.upto(row_start + (columns - 4)).map do |n|
+          (n..n + 3).to_a
+        end
+      end.flatten(1)
+    end
+
+    def vertical_wins
+      0.upto(last_index_for_vertical_win).map { |n| (n..n + (3 * columns)).step(columns).to_a }
+    end
+
+    def diagonal_wins
+      # TODO: 
+    end
+
+    def row_starts
+      0.step(last_index, columns)
+    end
+
+    def last_index_for_vertical_win
+      ((rows - 3) * columns) - 1
+    end
 end
 
 a = Game.new
 a.render_board
+binding.pry
--- lib/game_state.rb
+++ lib/game_state.rb
@@ -1,9 +1,13 @@
+require "pry"
+require_relative "./game"
 class GameState
-  attr_accessor :current_player, :board, :moves
+  attr_accessor :current_player, :board, :moves, :rows, :columns
 
-  def initialize(current_player, board)
+  def initialize(current_player, board, game)
     @current_player = current_player
-    @board = board
+    @board = board || Array.new(game.total_positions)
+    @rows = game.rows
+    @columns = game.columns
     @moves = []
   end
 
@@ -24,7 +28,7 @@ class GameState
   end
 
   def winner
-    @winner ||= winning_positions.map do |positions|
+    @winner ||= game.winning_positions.map do |positions|
       winning_row?(positions) || nil
     end.compact.first
   end
@@ -35,31 +39,6 @@ class GameState
 
   private
 
-    def winning_positions
-      [
-        horizontal_wins,
-        vertical_wins,
-        diagonal_wins
-      ].flatten(1)
-    end
-
-    def horizontal_wins
-      0.step(35, 7).map do |row_start|
-        row_start.upto(row_start + 3).map do |n|
-          (n..n + 3).to_a
-        end
-      end.flatten(1)
-    end
-
-    def vertical_wins
-      0.upto(20).map { |n| (n..n + 21).step(7).to_a }
-    end
-
-    def diagonal_wins
-      3.upto(20).map { |n| (n..n + 19).step(6).to_a }
-      3.upto(17).map { |n| (n..n + 25).step(8).to_a }
-      []
-    end
 
     def winning_row?(positions)
       player = board[positions[0]]
@@ -71,3 +50,5 @@ class GameState
       end
     end
 end
+a = GameState.new("B", nil, Game.new)
+binding.pry
